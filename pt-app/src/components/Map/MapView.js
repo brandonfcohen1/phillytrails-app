475bb6f3516f9c303d5a6aa050e2d548383a50a0
Brandon Cohen <brandon.f.cohen@gmail.com>
2021-08-07 21:04:44 -0400

    baselayer toggle

--- pt-app/src/components/Map/MapView.js
+++ pt-app/src/components/Map/MapView.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
-import {MapContainer, TileLayer, GeoJSON} from 'react-leaflet';
+import {MapContainer, TileLayer, GeoJSON, Popup, LayersControl, Marker} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-const mapboxURL = "https://api.mapbox.com/styles/v1/brandonfcohen/ckeykvju00t6b19phhhu7en4c/tiles/{" +
+const mapboxURL_streets = "https://api.mapbox.com/styles/v1/brandonfcohen/ckeykvju00t6b19phhhu7en4c/tiles/{" +
         "z}/{x}/{y}{r}?access_token=" + process.env.REACT_APP_MAPBOX;
+
+const mapboxURL_image = "https://api.mapbox.com/styles/v1/brandonfcohen/ckgpuunqr162319n1i6gq8z99/tiles/{" +
+"z}/{x}/{y}{r}?access_token=" + process.env.REACT_APP_MAPBOX;
+
 
 class MapView extends Component {
     constructor(props) {
@@ -19,13 +23,8 @@ class MapView extends Component {
     }
 
     componentDidMount() {
-        async function getTrails() {
-            return await fetch("/api/geojson").then(res => res.json());
-        }
 
-        getTrails().then(res => {
-            this.setState({trails: res})
-        });
+        fetch("/api/geojson").then(res => res.json()).then(res => {this.setState({trails: res})})
 
     }
 
@@ -38,27 +37,62 @@ class MapView extends Component {
                     center={[39.9741171, -75.1914883]}
                     zoom={13}
                     style={{
-                    height: "100vh"
-                }}>
-                    <TileLayer
-                        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
-                        url={mapboxURL}
-                        tileSize={512}
-                        zoomOffset={-1}/> {this.state.trails && (<GeoJSON
-                        data={this.state.trails}
-                        style={function (feature) {
-                        switch (feature.properties.type) {
-                            case 'paved trail':
-                                return {color: "#ff0000"};
-                            case 'paved road':
-                                return {color: "#0000ff"};
-                        }
-                    }}
-                        onEachFeature={function onEachFeature(feature, layer) {
-                        if (feature.properties && feature.properties.segment_description) {
-                            layer.bindPopup(feature.properties.segment_description);
-                        }
-                    }}/>)}
+                    height: "100vh"}
+                }>
+
+                    <LayersControl position="topright">
+                        <LayersControl.BaseLayer checked name="Streets">
+                            <TileLayer
+                                attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                                url={mapboxURL_streets}
+                                tileSize={512}
+                                zoomOffset={-1}/> 
+                        </LayersControl.BaseLayer>
+                        <LayersControl.BaseLayer name="Satellie">
+                            <TileLayer
+                                attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
+                                url={mapboxURL_image}
+                                tileSize={512}
+                                opacity={0.9}
+                                zoomOffset={-1}/> 
+                        </LayersControl.BaseLayer>
+                        <LayersControl.Overlay name="Marker with popup">
+                            <Marker position={[39.9741171, -75.1914883]}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                            </Marker>
+                        </LayersControl.Overlay>
+
+                        </LayersControl>
+                    
+                        
+                    {this.state.trails && (<GeoJSON
+                            data={this.state.trails}
+                            style={(feature) => {
+                                let style = {opacity: 0.8};
+                                switch (feature.properties.type) {
+                                    case 'paved trail':
+                                        style['color'] = "#454545";
+                                        break;
+                                    case 'paved road':
+                                        style['color'] = "#000000";
+                                        break;
+                                    case 'dirt trail':
+                                        style['color'] = "#FF0000";
+                                        break;
+                                    case 'dirt road':
+                                        style['color'] = "#800000";
+                                        break;
+                                    default:
+                                        style['color'] = "#FF0000";
+                                }
+                            return style;
+                        }}
+                            onEachFeature={(feature, layer) => {
+                                layer.bindPopup(feature.properties.segment_description);
+                            }
+                        }/>)}
 
                 </MapContainer>
             </div>
