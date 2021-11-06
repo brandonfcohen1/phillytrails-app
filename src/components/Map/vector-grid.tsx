a37e363663998bd6e32962267eb8d54e7b646362
Brandon Cohen <brandon.f.cohen@gmail.com>
2021-11-06 10:05:37 -0400

    vectorgrid typescript

--- src/components/Map/vector-grid.tsx
+++ src/components/Map/vector-grid.tsx
@@ -6,12 +6,12 @@ import L from "leaflet";
 import "leaflet.vectorgrid";
 
 
-export default function VectorGrid(props) {
+export default function VectorGrid() {
   const { layerContainer, map } = useLeafletContext();
 
   const martinUrl = 'http://localhost:3001/public.trails/{z}/{x}/{y}.pbf';
 
-  const vectorGrid = L.vectorGrid.protobuf(martinUrl, {onClick: ()=>{console.log("hello")}});
+  const vectorGrid = (L as any).vectorGrid.protobuf(martinUrl, {onClick: ()=>{console.log("hello")}});
 
   const container = layerContainer || map;
 
