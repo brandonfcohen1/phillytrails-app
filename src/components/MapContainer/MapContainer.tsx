072ca39c58a340a09a4b788abc659305edfd4e0c
Brandon Cohen <brandon.f.cohen@gmail.com>
2021-11-07 09:29:54 -0500

    fix too many requests

--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -53,23 +53,28 @@ const Map = (props: any) => {
   };
 
   useEffect(() => {
-    console.log(process.env.REACT_APP_API_URL)
     // load trails data
-    fetch(process.env.REACT_APP_API_URL + "/api/geojson/trails", {mode: 'cors'})
+    fetch(process.env.REACT_APP_API_URL + "/api/geojson/trails", {
+      mode: "cors",
+    })
       .then((res) => res.json())
       .then((res) => {
         setTrails(JSON.stringify(res));
       });
 
     // load transit lines data
-    fetch(process.env.REACT_APP_API_URL + "/api/geojson/transit_lines", {mode: 'cors'})
+    fetch(process.env.REACT_APP_API_URL + "/api/geojson/transit_lines", {
+      mode: "cors",
+    })
       .then((res) => res.json())
       .then((res) => {
         setLines(JSON.stringify(res));
       });
 
     // load transit stops data
-    fetch(process.env.REACT_APP_API_URL + "/api/geojson/transit_stops", {mode: 'cors'})
+    fetch(process.env.REACT_APP_API_URL + "/api/geojson/transit_stops", {
+      mode: "cors",
+    })
       .then((res) => res.json())
       .then((res) => {
         setStops(JSON.stringify(res));
@@ -81,9 +86,13 @@ const Map = (props: any) => {
       .then((res) => {
         setBikes(JSON.stringify(res));
       });
+  }, []);
 
+  useEffect(() => {
+    
     // get route details based on id, if /route/id is accessed
     if (props.id) {
+      console.log("fetch");
       fetch("/api/center/trail/" + props.id.id)
         .then((res) => res.json())
         .then((res) => {
