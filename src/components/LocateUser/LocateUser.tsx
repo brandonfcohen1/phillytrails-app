f8122eaa81126fa415081b39eabc21c0c859b3bb
Brandon Cohen <brandon.f.cohen@gmail.com>
2022-08-15 17:28:27 -0400

    add current location icon

--- src/components/LocateUser/LocateUser.tsx
+++ src/components/LocateUser/LocateUser.tsx
@@ -3,6 +3,8 @@ import "./LocateUser.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCrosshairs } from "@fortawesome/free-solid-svg-icons";
 import { Rings } from "react-loader-spinner";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 export const LocateUser = (map: any) => {
   const [finding, setFinding] = useState(false);
@@ -11,9 +13,18 @@ export const LocateUser = (map: any) => {
     map &&
       map.map.locate().on("locationfound", function (e: any) {
         map.map.flyTo(e.latlng, map.map.getZoom());
+        L.marker(e.latlng, { icon: basicIcon })
+          .bindPopup("Current Location")
+          .addTo(map.map);
         setFinding(false);
       });
   };
+
+  const basicIcon = L.icon({
+    iconUrl: "/marker_icon.png",
+    iconSize: [24, 36],
+  }) as any;
+
   return (
     <div className="location-button">
       <button onClick={setLocation}>
